5f1b61c69db0b1c2f8f66e3215a8f11ff08996a4
Emmanuel Yartey <supremeconquerorx@gmail.com>
2024-07-26 15:08:05 +0000

    Server component error fixed

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
- 
+ import LikeButton from "./like-button";
+
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>;
 }
@@ -7,11 +7,7 @@ function Header({ title }) {
 export default function HomePage() {
   const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
  
-  const [likes, setLikes] = useState(0);
- 
-  function handleClick() {
-    setLikes(likes + 1);
-  }
+
  
   return (
     <div>
@@ -21,8 +17,7 @@ export default function HomePage() {
           <li key={name}>{name}</li>
         ))}
       </ul>
- 
-      <button onClick={handleClick}>Like ({likes})</button>
+    <LikeButton/>
     </div>
   );
 }
